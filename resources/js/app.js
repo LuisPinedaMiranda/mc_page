4ebc53971755e7c9a9899afd8dbad4a16589e144
LuisPinedaMiranda <luis.pineda@cyberpeace.tech>
2024-04-26 20:39:28 -0600

    Todo lo del Swiper Propuestas

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,34 @@
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import.meta.glob(['../images/**'])
+
+import Swiper from 'swiper'
+import { Navigation, Pagination } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+Swiper.use([Navigation, Pagination])
+
+var swiper = new Swiper('.slide-content', {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlanck: true,
+  breakpoints: {
+    769: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
