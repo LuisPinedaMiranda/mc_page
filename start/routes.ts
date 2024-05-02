bbf68cfe78a2f7a9ef53b2caf3d975befb863ae5
LuisPinedaMiranda <luis.pineda@cyberpeace.tech>
2024-05-02 00:53:09 -0600

    Propuestas Added

--- start/routes.ts
+++ start/routes.ts
@@ -14,3 +14,7 @@ router.on('/').render('pages/home')
 router.get('/planilla', async ({ view }) => {
   return view.render('pages/planilla')
 })
+
+router.get('/propuestas', async ({ view }) => {
+  return view.render('pages/propuestas')
+})
